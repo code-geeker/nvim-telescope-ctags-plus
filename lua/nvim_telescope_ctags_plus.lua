bf38eb9a62b7a5d5a4f8ac50962f972b8c50cb55
Elliot <mr.elliot.se@gmail.com>
2024-04-09 12:42:51 +0800

    updated

--- lua/nvim_telescope_ctags_plus.lua
+++ lua/nvim_telescope_ctags_plus.lua
@@ -9,6 +9,11 @@ local action_set = require "telescope.actions.set"
 local make_entry = require "telescope.make_entry"
 local utils = require "telescope.utils"
 local conf = require("telescope.config").values
+local theme = require "telescope.themes"
+
+local entry_display = require "telescope.pickers.entry_display"
+local Path = require "plenary.path"
+
 
 local flatten = vim.tbl_flatten
 
@@ -33,6 +38,75 @@ local function visual_or_cword()
   end
   return vim.fn.expand('<cword>')
 end
+
+
+local function gen_from_ctags(opts)
+  opts = opts or {}
+  local cwd = vim.fn.expand(opts.cwd or vim.loop.cwd())
+
+  local displayer = entry_display.create {
+    separator = " ",
+    items = {
+      { remaining = true },
+    },
+  }
+
+  --[[ local make_display = function(entry)
+    local filename = utils.transform_path(opts, entry.filename)
+      return displayer {
+        filename,
+      }
+  end ]]
+
+
+  local make_display = function(entry)
+      local hl_group, icon
+      local display = utils.transform_path(opts, entry.filename)
+
+      display, hl_group, icon = utils.transform_devicons(entry.filename, display,  opts.disable_devicons)
+
+      if hl_group then
+        return display, { { { 0, #icon }, hl_group } }
+      else
+        return display
+      end
+    end
+
+
+  return function(line)
+    if line == "" or line:sub(1, 1) == "!" then
+      return nil
+    end
+
+    local tag, file, scode, lnum
+    -- ctags gives us: 'tags\tfile\tsource'
+    tag, file, scode = string.match(line, '([^\t]+)\t([^\t]+)\t/^?\t?(.*)/;"\t+.*')
+    if not tag then
+      -- hasktags gives us: 'tags\tfile\tlnum'
+      tag, file, lnum = string.match(line, "([^\t]+)\t([^\t]+)\t(%d+).*")
+    end
+
+    if Path.path.sep == "\\" then
+      file = string.gsub(file, "/", "\\")
+    end
+
+    -- local absolute_path = Path:new({file})
+    -- file = absolute_path:make_relative(cwd)
+
+    local tag_entry = {}
+
+    tag_entry.ordinal = file .. ":" .. tag
+    tag_entry.display = make_display
+    tag_entry.scode = scode
+    tag_entry.tag = tag
+    tag_entry.filename = file
+    tag_entry.col = 1
+    tag_entry.lnum = lnum and tonumber(lnum) or 1
+    -- print(vim.inspect(tag_entry))
+    return  tag_entry
+  end
+end
+
 
 ctags_plus.jump_to_tag = function(opts)
   opts = opts or {}
@@ -66,7 +140,10 @@ ctags_plus.jump_to_tag = function(opts)
     return
   end
 
-  opts.entry_maker = vim.F.if_nil(opts.entry_maker, make_entry.gen_from_ctags(opts))
+  -- opts.entry_maker = vim.F.if_nil(opts.entry_maker, make_entry.gen_from_ctags(opts))
+  opts.entry_maker = vim.F.if_nil(opts.entry_maker, gen_from_ctags(opts))
+  opts = theme.get_dropdown(opts)
+  -- print(vim.inspect(gen_from_ctags(opts)))
 
   pickers.new(opts, {
     prompt_title = "Tags for: " .. word,
